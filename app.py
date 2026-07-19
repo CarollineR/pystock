6a99e1460be88d7cd655e899ff6d4400205fed5e
Carolline Ramos <carollineramosc@gmail.com>
2026-07-19 14:36:13 -0300

    Adiciona primeira página HTML com Flask

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Olá, PyStock!"
+    return render_template("index.html")
