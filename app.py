d9e2c993e29dfdcba10cabdf70d2e4d2d69bff89
CarollineR <carollineramosc@gmail.com>
2026-07-21 19:14:24 -0300

    Implementa navegação, formulários e captura de dados no Flask

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
+
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        quantidade = request.form["quantidade"]
+        print(nome)
+        print(quantidade)
+        
+    return render_template("cadastro.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
